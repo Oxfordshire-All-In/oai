5e73e4ecd206c1fb13f19cb83f7993de92f68678
Mike Walmsley <walsleymk1@gmail.com>
2020-03-29 13:28:53 +0100

    Updates

---
+++ precache-manifest.b8e10054505ed659e88872712d17989d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f060d606ff98bc74673e73236bf891a",
+    "url": "/oai/index.html"
+  },
+  {
+    "revision": "5508a70896e4adab4b28",
+    "url": "/oai/static/css/main.1903bcfb.chunk.css"
+  },
+  {
+    "revision": "0e99352badae18ed4391",
+    "url": "/oai/static/js/2.f8ba33f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oai/static/js/2.f8ba33f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5508a70896e4adab4b28",
+    "url": "/oai/static/js/main.a8dba899.chunk.js"
+  },
+  {
+    "revision": "d9b8c84bbd4f31fafe99",
+    "url": "/oai/static/js/runtime-main.9601d59e.js"
+  }
+]);
