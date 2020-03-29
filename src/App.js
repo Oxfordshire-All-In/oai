ed4683165431d6cb9b39ad389b8237e5dfa79a83
Mike Walmsley <walsleymk1@gmail.com>
2020-03-29 22:09:00 +0100

    A little more spacing

--- src/App.js
+++ src/App.js
@@ -16,14 +16,15 @@ function App() {
           <h2> ArcGIS Embed Demo </h2>
           <div class="embed-container">
             <iframe
-            width="500" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Front End Map MW" 
+            width="800" height="640" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Front End Map MW" 
             src="https://www.arcgis.com/apps/Embed/index.html?webmap=c939500d9372422cb50cae76e1990de4&extent=-1.5681,51.4764,-0.8402,51.9435&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&legend=true&disable_scroll=true&theme=light">
             </iframe>
           </div>
 
-
           <h2> Leaflet/OSM Embed Demo </h2>
-          <SimpleExample ></SimpleExample>
+          <SimpleExample style={{width:"800", height:"640"}} ></SimpleExample>
+          <br></br>
+          <br></br>
 
 
 
